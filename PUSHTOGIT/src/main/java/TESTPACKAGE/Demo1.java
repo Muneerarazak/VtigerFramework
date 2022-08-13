abc84e7f2aa95be2195514c1ce1b1b29b0349230
Muneerarazak <shahnaz1612@gmail.com>
2022-08-13 16:41:26 +0530

    pushtogit

---
+++ PUSHTOGIT/src/main/java/TESTPACKAGE/Demo1.java
@@ -0,0 +1,23 @@
+package TESTPACKAGE;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	public class DemoTest 
+	{
+		@Test
+		public void method()
+		{
+			System.out.println("demo test running");
+		}
+
+		@Test
+		public void methodName()
+		{
+			System.out.println("demo1 test running");
+		}
+	}
+
+
+}
